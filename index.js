fad3a0952e6c6912980c8d3ed9e1dc29695891e8
AntonGordei <49307594+AntonGordei@users.noreply.github.com>
2020-10-20 15:33:04 +0300

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const list = require('./test')
 
 const getGraph = (list, obj = { identity: null }) => {
-    return list.filter(el => el.parentIdentity === obj.identity).map(newEl => ({
-        ...newEl,
-        children: getGraph(list, newEl)
-        })
+    return list
+            .filter(el => el.parentIdentity === obj.identity)
+            .map(newEl => {
+
+                // remove item
+                const index = list.indexOf(newEl)
+                if (index > -1) {
+                    list.splice(index, 1)
+                }
+                // ===========================
+
+                return {
+                        ...newEl,
+                        children: getGraph(list, newEl)
+                       }
+            }
     )
 }
 
